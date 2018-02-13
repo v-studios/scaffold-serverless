19f5384f4c2251a9ed6ada9442c7aee3f60e07ce
Chris Shenton <chris@v-studios.com>
2018-02-13 12:48:49 -0500

    Got it working, with Lambda role to write to DDB

--- app/handler.py
+++ app/handler.py
@@ -1,24 +1,26 @@
-import json
+import logging
+from urllib.parse import unquote_plus
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('uploads-info-dev')
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+for b in ('boto', 'boto3', 'botocore'):
+    logging.getLogger(b).setLevel(logging.WARNING)
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+def s3upload(event, context):
+    for record in event['Records']:
+        log.debug('event={}'.format(event))
+        s3name = record['s3']['bucket']['name']
+        obj = record['s3']['object']
+        key = unquote_plus(obj['key'])
+        size = int(obj['size'])
+        etag = obj.get('eTag')  # upload gives eTag but copy does not
+        log.info('bucket={} key={} size={} etag={}'.format(s3name, key, size, etag))
+        res = table.put_item(Item={'id': key, 'size': size, 'etag': etag})
+        if res['ResponseMetadata']['HTTPStatusCode'] != 200:
+            log.error('Putting item to DynamoDB res={}'.format(res))
