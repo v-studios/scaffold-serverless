402ff9ef8dac1f9cb455d4f6b37bb5490c05f9d7
Chris Shenton <chris@v-studios.com>
2018-02-26 18:57:49 -0500

    Return assets from DB as JSON or HTML if requested

--- app/handler.py
+++ app/handler.py
@@ -1,4 +1,5 @@
-import json
+import decimal
+from json import dumps
 import logging
 from os import environ
 from urllib.parse import unquote_plus
@@ -44,7 +45,7 @@ def get_upload_url(event, context):
     then set a variable 'url' to the returned value, and upload:
         curl -v --upload-file ~/Pictures/alex.jpg "$url"
     """
-    log.debug('event={}'.format(json.dumps(event)))
+    log.debug('event={}'.format(dumps(event)))
     log.debug('context aws_request_id={}'.format(context.aws_request_id))
     filename = event['queryStringParameters'].get('filename')
     if not filename:
@@ -53,9 +54,40 @@ def get_upload_url(event, context):
     url = s3.generate_presigned_url('put_object',
                                     Params={'Bucket': UPLOAD_BUCKET_NAME,
                                             'Key': filename,
-                                            # 'ContentType': content_type, from upload
                                             },
                                     ExpiresIn=3600)
     log.info('url={}'.format(url))
     return {'statusCode': 200,
-            'body': json.dumps({'url': url})}
+            'body': dumps({'url': url})}
+
+
+def get_assets(event, contex):
+    """Return assets listed in DB as JSON, or HTML if the request wanted text/html.
+
+    This uses a `scan` which is expensive and evil,
+    but for this toy project, no biggie.
+    We're not going to exhaustively scan if we hit DDB 1MB limit.
+    """
+    log.debug('event={}'.format(dumps(event)))
+    res = table.scan()
+    if res['ResponseMetadata']['HTTPStatusCode'] != 201:
+        return {'statusCode': 503, 'body': dumps({'error': res}, default=_undecimal)}
+    if 'text/html' not in event['headers']['Accept']:
+        return {'statusCode': 200, 'body': dumps(res['Items'], default=_undecimal)}
+    # Hack out some html, ick
+    html = '<table>\n'
+    for item in res['Items']:
+        html += '<tr><td>{id}</td><td>{size}</td></tr>\n'.format(**item)
+    html += '</table>'
+    return {'statusCode': 200,
+            'headers': {'Content-Type': 'text/html'},
+            'body': html}
+
+
+def _undecimal(obj):
+    """Work around: Object of type 'Decimal' is not JSON serializable: TypeError.
+    This is due to Dynamo returning things like 'size' as Decimal rather than int.
+    """
+    if isinstance(obj, decimal.Decimal):
+        return int(obj)
+    raise TypeError
