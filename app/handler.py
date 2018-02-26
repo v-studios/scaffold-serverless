d0f5c470700d461e1da9c0ecf34560a264863d33
Chris Shenton <chris@v-studios.com>
2018-02-26 17:23:40 -0500

    We get presigned URL and can now PUT an upload to it

--- app/handler.py
+++ app/handler.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from os import environ
 from urllib.parse import unquote_plus
@@ -15,10 +16,14 @@ log.info('DYNAMODB_UPLOAD_INFO_TABLE={}'.format(DYNAMODB_UPLOAD_INFO_TABLE))
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(DYNAMODB_UPLOAD_INFO_TABLE)
 
+UPLOAD_BUCKET_NAME = environ['UPLOAD_BUCKET_NAME']
+s3 = boto3.client('s3')
+
 
 def s3upload(event, context):
     for record in event['Records']:
-        log.debug('event={}'.format(event))
+        log.info('event={}'.format(event))
+        log.info('context={}'.format(context))
         s3name = record['s3']['bucket']['name']
         obj = record['s3']['object']
         key = unquote_plus(obj['key'])
@@ -28,3 +33,32 @@ def s3upload(event, context):
         res = table.put_item(Item={'id': key, 'size': size, 'etag': etag})
         if res['ResponseMetadata']['HTTPStatusCode'] != 200:
             log.error('Putting item to DynamoDB res={}'.format(res))
+
+
+def get_upload_psk(event, context):
+    """Return a presigned key URL to POST a file to.
+
+    Test like:
+        curl -i  "$urlget"?filename=ALEX.JPG
+
+    then set a variable 'url' to the returned value, and upload:
+        curl -v --upload-file ~/Pictures/alex.jpg "$url"
+    """
+    # TODO: accept form fields like: curl --form filename=ALEX.JPG
+    # May have to do with a POST Lambda handler
+    log.debug('event={}'.format(json.dumps(event)))
+    log.debug('context aws_request_id={}'.format(context.aws_request_id))
+    filename = event['queryStringParameters'].get('filename')
+    if not filename:
+        return {'statusCode': 400,
+                'body': 'Must supply query string "filename=..."'}
+    url = s3.generate_presigned_url('put_object',
+                                    Params={'Bucket': UPLOAD_BUCKET_NAME,
+                                            'Key': filename,
+                                            # 'ContentType': content_type, from upload
+                                            },
+                                    ExpiresIn=3600)
+    log.info('url={}'.format(url))
+    return {'statusCode': 200,
+            'body': json.dumps({'url': url})}
+
