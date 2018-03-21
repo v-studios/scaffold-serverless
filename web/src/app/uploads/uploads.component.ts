bd7b3c65e4c91b7d11139b9991bd0368267377ac
Chris Shenton <chris@v-studios.com>
2018-03-21 18:08:14 -0400

    Upload services are kinda working but S3 PSURL is forbidden, bad sig
    I had to fold the upload into the hander of the get_psurl else I lost
    it in the scope somewhere (much to learn)

--- web/src/app/uploads/uploads.component.ts
+++ web/src/app/uploads/uploads.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { MessageService } from '../message.service';
 import { Upload } from '../upload';
+import { UploadURL } from '../upload-url';
 import { UploadService } from '../upload.service';
 
 @Component({
@@ -36,34 +37,35 @@ export class UploadsComponent implements OnInit {
     this.uploadService.getUploads().subscribe(uploads => this.uploads = uploads);
   }
 
-  // TODO: on uploads page, get presigned URL from API then PUT file to that S3 URL
-  // widget should be file chooser with action to invoke this `add`
-  // which should GET a presigned URL then PUT to it.
-  add(id: string): void {
-    id = id.trim();
-    if (!id) { return; }
-    // I don't have to instantiate all Upload attrs, they become '' or none, don't know yet
-    this.uploadService.addUpload({ id } as Upload)
-      .subscribe(upload => {
-        this.uploads.push(upload);
-      });
-    this.log(`add added id=${id}`);
+  // // TODO: on uploads page, get presigned URL from API then PUT file to that S3 URL
+  // // widget should be file chooser with action to invoke this `add`
+  // // which should GET a presigned URL then PUT to it.
+  // add(id: string): void {
+  //   id = id.trim();
+  //   if (!id) { return; }
+  //   // I don't have to instantiate all Upload attrs, they become '' or none, don't know yet
+  //   this.uploadService.addUpload({ id } as Upload)
+  //     .subscribe(upload => {
+  //       this.uploads.push(upload);
+  //     });
+  //   this.log(`add added id=${id}`);
+  // }
 
-  }
-
-  // TODO: on uploads page, get presigned URL from API then PUT file to that S3 URL
-  // widget should be file chooser with action to invoke this `add`
-  // which should GET a presigned URL then PUT to it.
+  // On uploads page, get presigned URL from API then PUT file to that S3 URL
   add2(filename: string): void {
     filename = filename.trim();
     if (!filename) { return; }
-    // TODO: get the filename without preceeding path
-    this.uploadService.getUploadURL(filename) // as string ??
-      .subscribe(url => {
-        this.log(`add2 got url=${url}`);
-      })
-    this.log(`add2 added filename=${filename}`);
-
+    // TODO: get the filename without preceeding path or HTML5 File object
+    this.uploadService.getUploadURL(filename)
+      .subscribe(urlObj => {
+        this.log(`add2 got urlObj.url=${urlObj.url}`);
+        // local copy of urlObj seems undefined outside this scope, do here while we have it
+        // or maybe it's executing async, so runs before we set it?
+        this.uploadService.putUploadFile(urlObj, 'BODY FROM COMPONENT')
+          .subscribe(res => {       // probably no response
+            this.log('add2 upload res=${res}');
+          });
+      });
   }
 
   delete(upload: Upload): void {
